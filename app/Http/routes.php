86ab4b2c32b84e15bb5a8ab4739ce5795fd9f56a
mengkongly <mengkong2009@yahoo.com>
2018-11-02 18:13:38 -0500

    Insert post and read post

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,9 +17,29 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/user/{id}/posts',function($id){
+/* Route::get('/user/{id}/posts',function($id){
 
     //return User::findOrFail($id)->posts;
     $user   =    User::findOrFail($id);
     return $user->posts;
+}); */
+
+Route::get('/user/{id}/posts','PostController@index');
+//Route::post('/user/{id}/post','PostController@store');
+Route::get('/user/{id}/in_post',function($id){
+    $user   =   User::findOrFail($id);
+
+    $post      =    new Post(["title"=>"New title 2","body"=>"Content of new title 2"]);
+    /* $post->title="New title";
+    $post->body="Content of new title"; */
+
+    $user->posts()->save($post);
 });
+
+//get by user id and post id
+Route::get('/user/{id}/post/{post_id}',function($id,$post_id){
+
+    $user   =   User::where('id',$id)->first();
+    return $user->posts()->where('id',$post_id)->get();
+});
+
